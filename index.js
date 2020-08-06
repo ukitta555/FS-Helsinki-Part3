cb9ba71faffb42c5b8bfd73eb361a7094d1efa0e
Vlad Nekriach <ukitta555@gmail.com>
2020-08-06 14:33:39 -0100

    adding frontend to the prod build

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.use(cors())
 app.use (express.json())
-
+app.use(express.static('build'))
 
 morgan.token ('bodyOutput', (request, result) =>
                                                 {
