12eaa6b3434f56e807b6cf0e544c2b74ef4d8241
Vlad Nekriach <ukitta555@gmail.com>
2020-08-17 15:54:59 -0100

    completed exercise 3.16

--- index.js
+++ index.js
@@ -27,11 +27,12 @@ app.get ('/', (request, response) =>
                                     }
         )
 
-app.get ('/api/persons', (request, response) =>
-                                              {
-                                                Person.find({})
-                                                      .then (people => response.json(people))
-                                              }
+app.get ('/api/persons', (request, response, next) =>
+                                                    {
+                                                      Person.find({})
+                                                            .then (people => response.json(people))
+                                                            .catch(error => next(error))
+                                                    }
         ) 
 
 app.get ('/info', (request, response) =>
@@ -58,7 +59,7 @@ app.get ('/api/persons/:id', (request, response) =>
                                                   }
         )
 
-app.delete('/api/persons/:id', (request, response) =>
+app.delete('/api/persons/:id', (request, response, next) =>
                                                       {
                                                         Person.findByIdAndDelete(request.params.id)
                                                               .then(result =>
@@ -66,13 +67,13 @@ app.delete('/api/persons/:id', (request, response) =>
                                                                               response.status(204).end()
                                                                             } 
                                                                    )
-                                                              .catch(error => console.log('error occured while deleting', error.message))
+                                                              .catch(error => next(error))
                                                       }
           )
 
 
 
-app.post ('/api/persons', (request, response) =>
+app.post ('/api/persons', (request, response, next) =>
                                               {
                                                 const body = request.body
                                                 if (!body.name || !body.phone)
@@ -105,7 +106,7 @@ app.post ('/api/persons', (request, response) =>
                                                       .then (savedPerson => {
                                                                               response.json(savedPerson)
                                                                             })
-                                                      .catch(error => console.log (`error occurred:`, error.message))
+                                                      .catch(error =>next(error))
                                               } 
          )
 
@@ -117,7 +118,20 @@ const unknownEndpoint = (request, response) =>
                                               }
 
 app.use(unknownEndpoint)
-const port = process.env.PORT || 3001
+
+const errorHandler = (error, request, response, next) => 
+                                                        {
+                                                          console.log (error.message,'handler')
+                                                          if (error.name === 'CastError')
+                                                          {
+                                                            return response.status(400).send({error: 'malformatted id'})
+                                                          }
+                                                          next(error)
+                                                        }
+
+app.use (errorHandler)                                                        
+
+const port = process.env.PORT
 app.listen (port, () => 
                         {
                           console.log (`App running on port ${port}`)
